3ba0ba080199f5cb0b6d5244e662c027a05c8d30
natalia_nn2020 <sc20nn@leeds.ac.uk>
2022-02-16 10:10:24 +0000

    basic database created

--- app/db_create.py
+++ app/db_create.py
@@ -1,5 +1,5 @@
 from config import SQALCHEMY_DATABASE_URI
-from app import db_create
+from app import db
 import os.path
 
 db.create_all()
--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
 # file for the database
 
 from app import db
+
+# basic models for now, relationships to be added later
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    #card details - have a separate model ?
+    #booking = will have a relationship with Scooters via the Bookings model
+
+class Scooters(db.Model):
+    __tablename__ = 'scooters'
+    id = db.Column(db.Integer, primary_key=True)
+    available = db.Column(db.Integer) # 0 if available, 1 if being used
+    # location - will have a relationship with the Location model
+
+class Locations(db.Models):
+    __tablename__ = 'locations'
+    id = db.Column(db.Integer, primary_key=True)
+    # available scooters - will have a relationship with the Scooters model
+
+class Staff(db.Model):
+    __tablename__ = 'staff'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(100))
+
+class Bokings(db.Model):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    # other fields will come from Users-Scooters relationship
+
+#store rates and calculate price for each booking
+#draft version, to be confirmed
+class Prices(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    duration = db.Column(db.Integer)
+    cost = db.Column(db.Integer)
+
