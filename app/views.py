7da72f775fb43fbf5d97ac2c9b1ff75301b28ce3
Zhanpeng Zhu <ml199zz@leeds.ac.uk>
2022-02-24 12:54:10 +0000

    Sprint 1 redirection and form validation

--- app/views.py
+++ app/views.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
-from app import app
-from .forms import SignupForm, LoginForm
+from flask import Flask, request, redirect, url_for, render_template
+from forms import LoginForm, SignupForm
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('base.html', title="Base Page")
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = SignupForm()
-    return render_template('signup.html', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        form = LoginForm(request.form)
+        # get and check input login information form, if true then redirect to the homepage
+        if form.validate():
+            return redirect("/staff")
+        else:
+            return "Your input format is wrong!"
+
+
+@app.route("/staff", methods=['GET', 'POST'])
+def staff():
+    if request.method == 'GET':
+       return render_template("staff.html")
+
+
+@app.route("/user", methods=['GET', 'POST'])
+def user():
+    if request.method == 'GET':
+       return render_template("user.html")
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm(request.form)
+    if request.method == 'GET':
+       return render_template("signup.html")
+    else:
+        # get and check input login information form, if true then signup successfully
+        if form.validate():
+            name = form.name.data
+            age = form.age.data
+            email = form.email.data
+            password = form.password.data
+            return "signup successfully!"
+        else:
+            return "Your input format is wrong!"
+
+
+if __name__ == '__main__':
+    app.run()
