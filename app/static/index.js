f7c69cc4f5c2aa9b62af8b7a5ae4bd3f6a6bc705
sc20soj <sc20soj@leeds.ac.uk>
2022-03-23 08:54:33 +0000

    Testing, maps files

--- app/static/index.js
+++ app/static/index.js
@@ -33,10 +33,36 @@ $('.tab a').on('click', function (e) {
 });
 
 
-let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function showLocation() {
+  var mapDict = {
+    "Trinity centre" : ["LS1 5AT"],
+    "Train Station": ["LS1 4DY"],
+    "Merrion centre": ["LS2 8NG"],
+    "LRI hospital": ["LS1 3EX"],
+    "UoL Edge sports centre": ["LS2 9JT"]
+  };
+  var geocoder = new google.maps.Geocoder();
+  
+  geocoder.geocode({
+    address: mapDict[document.getElementById('location').value]+""
+  }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      //map.setCenter(results[0].geometry.location); //center the map over the result
+
+      var mapOptions = {
+        zoom: 15,
+        center: results[0].geometry.location
+      }
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+      //place a marker at the location
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+      //markers.push(marker);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
--- testing.py
+++ testing.py
@@ -1,8 +1,12 @@
 import os
 import unittest
-from flask import Flask
+from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
 from app import app, db, models
+
+from flask_login import login_user, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -20,24 +24,58 @@ class TestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_addtaskroute(self):
+    def test_routes(self):
         print("Test routes:")
         self.assertEqual(self.app.get('/signup', follow_redirects=True).status_code, 200)
+        self.assertEqual(self.app.get('/', follow_redirects=True).status_code, 200)
         self.assertEqual(self.app.get('/login', follow_redirects=True).status_code, 200)
+        self.assertEqual(self.app.get('/staff', follow_redirects=True).status_code, 200)
+        self.assertEqual(self.app.get('/add_scooter', follow_redirects=True).status_code, 200)
+        self.assertEqual(self.app.get('/add_price', follow_redirects=True).status_code, 200)
+        self.assertEqual(self.app.get('/signup', follow_redirects=True).status_code, 200)
+        self.assertEqual(self.app.get('/price_view', follow_redirects=True).status_code, 200)
+
 
     def test_valid_user_registration(self):
+        print("Testing registration")
         response = self.register('Scott', 19, 'scottJames44@gmail.com', 'Pa55W0rd')
         self.assertEqual(response.status_code, 200)
 
     def test_valid_user_login(self):
-        response = self.login('Scott', 'Pa55W0rd')
+        print("Testing login")
+        response = self.login('scottJames44@gmail.com', 'Pa55W0rd')
         self.assertEqual(response.status_code, 200)
 
+    def login(self, email, password):
+        return self.app.post('/login', data=dict(email=email, password=password))
+        
     def register(self, name, age, email, password):
-        return self.app.post( '/signup', data=dict(name=name, age=age, email=email, password=password), follow_redirects=True)
- 
-    def login(self, name, password):
-        return self.app.post('/login', data=dict(name=name, password=password), follow_redirects=True)
+        return self.app.post( '/signup', data=dict(name=name, age=age, email=email, password=password, pw_confirm=password), follow_redirects=True)
+    
+    def BookingForm(self, duration, location):
+        return self.app.post('/booking', data=dict(duration=duration, location=location))
+
+    def ScooterForm(self, available, location):
+        return self.app.post('/add_scooter"', data=dict(available=available, location=location))
+
+    def PriceForm(self, cost):
+        return self.app.post('/config_price/<id>"', data=dict(cost=cost))
+
+    def AddPriceForm(self, duration, cost):
+        return self.app.post('/add_price"', data=dict(duration=duration, cost=cost))
+
+    def CardForm(self, number, expiration_date, security_code, name, save):
+        return self.app.post('/add_price"', data=dict(number=number, expiration_date=expiration_date, security_code=security_code, name=name, save=save))
+
 
 if __name__ == '__main__':
     unittest.main()
+
+    #def test_booking_routes(self):
+        #with self.app:
+            #print("test_booking_routes:")
+            #response = self.login('fecdc@gmail.com', 'fexbex')
+            #print("Results of test",self.app.post('/login', data=dict(email='fecdc@gmail.com', password='fexbex'), follow_redirects=True))
+
+            #self.app.post.LoginForm().submit()
+            #self.assertEqual(self.app.get('/booking', follow_redirects=True).status_code, 200)
